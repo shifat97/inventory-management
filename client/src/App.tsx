5bb2fbdd781da0324c4fbc8800834700bcddb5fd
shifat97 <shifatbinreza2@gmail.com>
2026-01-04 21:11:10 +0600

    add: shadcn configuration

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 function App() {
   return (
     <div className="App">
       <h1 className="text-yellow-800 text-bold text-4xl">Hello World</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
