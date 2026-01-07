1fa32464e4e539cff53ab8c4b101ee6849728559
shifat97 <shifatbinreza2@gmail.com>
2026-01-07 13:42:36 +0600

    WIP

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,10 @@
 import { Button } from "@/components/ui/button";
 
 function App() {
+  const portNumber = import.meta.env.VITE_APP_PORT_NUMBER;
+
+  console.log(portNumber);
+
   return (
     <div className="App">
       <h1 className="text-yellow-800 text-bold text-4xl">Hello World</h1>
