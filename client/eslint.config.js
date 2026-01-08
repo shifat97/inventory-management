3e99f12889dd8f570ae3d27b4df3c99a0b41c92e
shifat97 <shifatbinreza2@gmail.com>
2026-01-08 09:58:07 +0600

    add: eslint config

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -8,7 +8,15 @@ import { defineConfig, globalIgnores } from "eslint/config";
 export default defineConfig([
   globalIgnores(["dist"]),
   {
-    files: ["**/*.{ts,tsx}"],
+    ignores: [
+      'components/ui/**', // ✅ Ignore all shadcn/ui files
+    ],
+  },
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    rules: {
+      'react-refresh/only-export-components': 'off',
+    },
     extends: [
       js.configs.recommended,
       tseslint.configs.recommended,
