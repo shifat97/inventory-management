b913fccc201c20a0b11dda01cdadabf62cbb37e5
shifat97 <shifatbinreza2@gmail.com>
2026-01-13 20:29:33 +0600

    client: remove unnecessary logs messages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,7 @@
-import { Button } from "@/components/ui/button";
-import { Outlet } from "react-router";
+import { Button } from '@/components/ui/button';
+import { Outlet } from 'react-router';
 
 function App() {
-  const portNumber = import.meta.env.VITE_PORT_NUMBER;
-
-  console.log(portNumber);
-
   return (
     <div className="App">
       <h1 className="text-yellow-800 text-bold text-4xl">Hello World</h1>
