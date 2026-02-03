8110e25e29cc3efde36c151434156c70aa6bd04a
shifat97 <shifatbinreza2@gmail.com>
2026-02-03 22:35:53 +0600

    client: move common files to utils

--- client/src/pages/users/updateUser.tsx
+++ client/src/pages/users/updateUser.tsx
@@ -1,6 +1,25 @@
 import { UserForm } from '@/pages/users/_components';
+import type { UserFormValues } from './types';
+import http from '@/lib/http';
+import { getUserFromLS } from '@/utils';
 
 export const UpdateUser = () => {
+  const onSubmit = (data: UserFormValues) => {
+    // UPDATE MODE
+    const payload = Object.fromEntries(
+      Object.entries(data).filter(
+        ([_, value]) => value !== '' && value !== undefined,
+      ),
+    );
+
+    const user = getUserFromLS();
+
+    http
+      .put(`/api/admin/users/${user._id}`, payload)
+      .then(() => alert('User updated successfully'))
+      .catch(console.error);
+  };
+
   return (
     <div className="w-screen h-scree">
       <UserForm
@@ -11,6 +30,7 @@ export const UpdateUser = () => {
           password: '',
           role: '',
         }}
+        onSubmit={onSubmit}
       />
     </div>
   );
